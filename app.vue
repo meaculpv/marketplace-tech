5ea2bac3092bf7438d5533c3ccb544c472072f15
Batyr <olzhabaevbatyr@gmail.com>
2024-05-22 02:02:05 +0500

    project init

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Products</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/favorites">Favorites</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/bucket">Bucket</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/bucket.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Bucket page</h1>
+  </div>
+</template>
