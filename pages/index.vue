e7b44b009e9128bd23a5ba58a52ea25eace4594d
Batyr <olzhabaevbatyr@gmail.com>
2024-05-22 05:02:24 +0500

    Add product card styles

--- pages/index.vue
+++ pages/index.vue
@@ -9,20 +9,31 @@
   });
 </script>
 <template>
-  <h1>Home page</h1>
-  <section class="section-products">
-    <div v-for="product in products" :key="product.id" class="product">
-      <div class="product__header">
-        <img :src="product.image" alt="" class="product__image" />
+  <div class="home">
+    <aside class="filters">
+      <div class="category">
+        <ul class="category__list">
+          <li class="category__item">Men's clothing</li>
+          <li class="category__item">Women's clothing</li>
+          <li class="category__item">Jewelery</li>
+          <li class="category__item">Electronics</li>
+        </ul>
       </div>
-      <div class="product__body">
-        <p class="product__price">{{ product.price }}</p>
-        <h3 class="product__title">{{ product.title }}</h3>
+    </aside>
+    <section class="section-products">
+      <div v-for="product in products" :key="product.id" class="product">
+        <div class="product__header">
+          <img :src="product.image" alt="" class="product__image" />
+        </div>
+        <div class="product__body">
+          <p class="product__price">${{ product.price }}</p>
+          <h3 class="product__title">{{ product.title }}</h3>
+        </div>
+        <div class="product__footer">
+          <FavoriteIcon class="product__action" />
+          <CartIcon class="product__action" />
+        </div>
       </div>
-      <div class="product__footer">
-        <FavoriteIcon />
-        <CartIcon />
-      </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
